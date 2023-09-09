177375e269bc2274241aa163b60c408a2d92184e
Natanael <natanael.santana@outlook.com.br>
2023-09-09 08:16:49 -0300

    api request changes

--- frontend/src/services/productServices.js
+++ frontend/src/services/productServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://dev-test-lbx1.onrender.com";
 
 export const getProducts = async () => {
   try {
